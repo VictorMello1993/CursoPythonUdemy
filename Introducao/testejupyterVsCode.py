651aa39f725604e1809362eee55098cafee88009
Victor Mello <victorsmello93@gmail.com>
2020-01-31 17:40:31 -0300

    Modificando o código para testar o modo markdown no VS Code

--- Introducao/testejupyterVsCode.py
+++ Introducao/testejupyterVsCode.py
@@ -1,6 +1,12 @@
-#%%
-1 + 2
-2 / 3
-20 // 7
-
 # %%
+5 ** 2
+# %%
+_ ** 3
+#%% [markdown]
+# ## Operadores
+#Lista de operadores
+print(1 + 2)
+print(2 / 3)
+print(20 // 7)
+print(5 ** 5)
+print(100 - 75)
