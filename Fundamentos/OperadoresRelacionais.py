ffe539893b27bfe91c48d56b7d794946e58cce43
Victor Mello <victorsmello93@gmail.com>
2020-02-01 15:53:01 -0300

    Operadores aritméticos e Operadores relacionais

---
+++ Fundamentos/OperadoresRelacionais.py
@@ -0,0 +1,10 @@
+# %% [markdown]
+## Operadores relacionais
+
+print(3 > 4) # Maior 
+print(4 >= 3) # Maior ou igual
+print(1 < 2) # Menor
+print(3 <= 1) # Menor ou igual
+print(3 != 2) # Diferente de
+print(3 == 3) # Igualdade 
+print(2 == '2') 
