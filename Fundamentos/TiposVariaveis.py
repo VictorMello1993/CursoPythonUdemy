1ce4b8853e07ed1534ef302f0f4f2c2a9bf0494a
Victor Mello <victorsmello93@gmail.com>
2020-02-01 14:15:47 -0300

    Tipos de variáveis

---
+++ Fundamentos/TiposVariaveis.py
@@ -0,0 +1,30 @@
+# %% [markdown]
+# ## Tipos básicos
+# %% [markdown]
+# Boolean
+
+print(True)
+print(False)
+# %% [markdown]
+# Float
+print(1.2 + 1)
+# %% [markdown]
+# Strings
+print('Olá mundo!')
+# %% [markdown]
+# Concatenação
+print('Você é ' + 3 * 'muito ' + 'legal!')
+#print(3 + '3') #Erro! Ambiguidade: 
+               #Não é possível realizar a operação de 
+               #concatenação misturando int com string
+# %% [markdown]
+# listas
+print([1, 2, 3])
+
+# %% [markdown]
+# dicionários (chave: valor)
+print({'Nome': 'Victor', 'Idade': 26, 'Profissao': 'Programador'})
+
+# %% [markdown]
+# Tipo nulo (None)
+print(None)
