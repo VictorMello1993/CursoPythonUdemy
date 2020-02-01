7e5c06f481fd99363460f710b0ede454de5e787c
Victor Mello <victorsmello93@gmail.com>
2020-02-01 15:05:27 -0300

    Operadores aritméticos

---
+++ Fundamentos/OperadoresAritmeticos.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+# ## Operadores aritméticos
+
+print(2 + 3) # Soma
+print(4 - 7) # Subtração
+print(2 * 5.3) # Multiplicação
+print(9.4 / 3) # Divisão
+print(9.4 // 3) # Divisão (truncado)
+print(2 ** 8) # Exponenciação
+print(10 % 4) # Resto da divisão
+
+a = 12
+b = a
+print(a + b)
