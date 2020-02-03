dcc08e8749d251d71471bb5eea7fd7eb1729082b
Victor Mello <victorsmello93@gmail.com>
2020-02-03 01:02:09 -0300

    Operadores unários

---
+++ Fundamentos/OperadoresUnarios.py
@@ -0,0 +1,12 @@
+#%% [markdown]
+## Operadores unários
+
+a = -3
+a += 1 #Neste caso, está sendo executado o incremento/decremento
+# a++ #Erro! Não suportado pelo Python
+++a """No Python, a notação pré-fixada indica que foi utilizado
+       mais de uma vez o operador unário para indicar a polaridade
+       de um valor numérico (positivo ou negativo), não realiza
+       o incremento/decremento como ocorre em outras
+       linguagens"""
+       
