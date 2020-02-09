dbeaac5433b60f8312d09391ebca2daf5aaf4e53
Victor Mello <victorsmello93@gmail.com>
2020-02-09 17:55:58 -0300

    Outro exemplo de if-else

---
+++ EstruturasControle/ExemploIfElse.py
@@ -0,0 +1,17 @@
+def faixa_etaria(idade):
+    if idade in range(0, 17):
+        return 'Menor de idade'
+    elif idade in range(18, 64):
+        return 'Adulto'
+    elif idade in range(65, 100):
+        return 'Melhor idade'
+    elif idade >= 100:
+        return 'Centenário'
+    else:
+        return 'Idade inválida!'
+
+if __name__ == '__main__':
+    idades = (2, 26, 30, 65, -113, 120)
+    for idade in idades:
+        print(faixa_etaria(idade))                                                
+        
