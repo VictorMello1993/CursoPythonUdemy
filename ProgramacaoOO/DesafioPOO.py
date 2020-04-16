c140ba174b349c289943f966a377d07704600368
Victor Mello <victorsmello93@gmail.com>
2020-04-16 19:50:42 -0300

    Resolução do desafio POO

--- ProgramacaoOO/DesafioPOO.py
+++ ProgramacaoOO/DesafioPOO.py
@@ -16,7 +16,11 @@ class Pessoa:
 class Vendedor(Pessoa):
     def __init__(self, nome, idade, salario):
         super().__init__(nome, idade)
-        self.salario = salario            
+        self.salario = salario
+
+    def __str__(self):
+        if isinstance(self, Vendedor):
+            return f'Nome do vendedor: {self.nome} - Idade: {self.idade} - Salário: {self.salario}'
 
 
 class Cliente(Pessoa):
@@ -29,6 +33,16 @@ class Cliente(Pessoa):
 
     def get_data_ultima_compra(self):
         return max(compra.data for compra in self.compras)
+
+    def __str__(self):
+        if isinstance(self, Cliente):
+            return f'Nome do cliente: {self.nome} - Idade: {self.idade}'
+
+    def total_compras(self):
+        total = 0
+        for compra in self.compras:
+            total += compra.valor
+        return total
 
 
 class Compra:
@@ -44,9 +58,10 @@ def main():
     vendedor2 = Vendedor('Igor Borges', 27, 3500.00)
     vendedor3 = Vendedor('Camilla Batista', 25, 3000.00)
 
-    cliente1.registrar_compra(Compra(vendedor1, datetime(2020, 4, 15), 25.50))
+    cliente1.registrar_compra(Compra(vendedor1, datetime(2020, 4, 15), 25.99))
     cliente1.registrar_compra(Compra(vendedor2, datetime(2021, 4, 30), 99.90))
-    cliente1.registrar_compra(Compra(vendedor3, datetime(2020, 12, 30), 40.00))
+    cliente1.registrar_compra(Compra(vendedor3, datetime(2020, 12, 30), 40.55))
+
 
     print(cliente1)
     print(vendedor1)
@@ -54,6 +69,7 @@ def main():
     print(vendedor3)
 
     print(cliente1.get_data_ultima_compra())
+    print('{:.2f}'.format(cliente1.total_compras()))
 
 
 if __name__ == "__main__":
